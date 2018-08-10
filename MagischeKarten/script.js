77825c9746a844f59fb2a4c818f55dc24fb06bcd
Marcus <marcus.freund@web.de>
2018-08-10 13:06:26 +0200

    Version 1.05
    - HTML:
    -- Meta Daten eingefuegt
    -- "Karte" eingebaut
    
    - JavaScript:
    -- Zahlen-Arrays sortiert
    -- formatierte Ausgabe der Zahlen-Arrays auf der "Karte"

--- MagischeKarten/script.js
+++ MagischeKarten/script.js
@@ -10,101 +10,107 @@
  *
  * @author Marcus Freund
  * @date 2018-08-06
- * @version 1.00
+ * @version 1.05
  */
 
-const cardArray1 = Array(
-                     
-					1, 9, 17, 25, 33,
-					41, 49, 57, 3, 11,
-					19, 27, 35, 43, 51,
-					59, 5, 13, 21, 29,
-					37, 45, 53, 61, 7,
-					15, 23, 31, 39, 47,
-					55, 63
-					
-                    );
+const cardArray1 = [
+	
+	1, 3, 5, 7, 
+	9, 11, 13, 15, 
+	17, 19, 21, 23, 
+	25, 27, 29, 31, 
+	33, 35, 37, 39, 
+	41, 43, 45, 47, 
+	49, 51, 53, 55, 
+	57, 59, 61, 63
+	
+	];
 
-const cardArray2 = Array(
-                     
-					2, 10, 18, 26, 34,
-					42, 50, 58, 3, 11,
-					19, 27, 35, 43, 51,
-					59, 6, 14, 22, 30,
-					38, 46, 54, 62, 7,
-					15, 23, 31, 39, 47,
-					55, 63
-					
-);
+const cardArray2 = [
+	
+	2, 3, 6, 7, 
+	10, 11, 14, 15, 
+	18, 19, 22, 23, 
+	26, 27, 30, 31, 
+	34, 35, 38, 39, 
+	42, 43, 46, 47, 
+	50, 51, 54, 55, 
+	58, 59, 62, 63	
+	
+	];
 
-const cardArray3 = Array(
-                     
-					4, 12, 20, 28, 36,
-					44, 52, 60, 5, 13,
-					21, 29, 37, 45, 53,
-					61, 6, 14, 22, 30,
-					38, 46, 54, 62, 7,
-					15, 23, 31, 39, 47,
-					55, 63
-					
-);
+const cardArray3 = [
+	
+	4, 5, 6, 7, 
+	12, 13, 14, 15, 
+	20, 21, 22, 23, 
+	28, 29, 30, 31, 
+	36, 37, 38, 39, 
+	44, 45, 46, 47, 
+	52, 53, 54, 55, 
+	60, 61, 62, 63	
+	
+	];
 
-const cardArray4 = Array(
-                     
-					8, 12, 24, 28, 40,
-					44, 56, 60, 9, 13,
-					25, 29, 41, 45, 57,
-					61, 10, 14, 26, 30,
-					42, 46, 58, 62, 11,
-					15, 27, 31, 43, 47,
-					59, 63
-					
-);
+const cardArray4 = [
+	
+	8, 9, 10, 11, 
+	12, 13, 14, 15, 
+	24, 25, 26, 27, 
+	28, 29, 30, 31, 
+	40, 41, 42, 43, 
+	44, 45, 46, 47, 
+	56, 57, 58, 59, 
+	60, 61, 62, 63
+	
+	];
 
-const cardArray5 = Array(
-                     
-					16, 20, 24, 28, 48,
-					52, 56, 60, 17, 21,
-					25, 29, 49, 53, 57,
-					61, 18, 22, 26, 30,
-					50, 54, 58, 62, 19,
-					23, 27, 31, 51, 55,
-					59, 63
-					
-);
+const cardArray5 = [
+	
+	16, 17, 18, 19, 
+	20, 21, 22, 23, 
+	24, 25, 26, 27, 
+	28, 29, 30, 31, 
+	48, 49, 50, 51, 
+	52, 53, 54, 55, 
+	56, 57, 58, 59, 
+	60, 61, 62, 63
+	
+	];
 
-const cardArray6 = Array(
-                     
-					32, 36, 40, 44, 48,
-					52, 56, 60, 33, 37,
-					41, 45, 49, 53, 57,
-					61, 34, 38, 42, 46,
-					50, 54, 58, 62, 35,
-					39, 43, 47, 51, 55,
-					59, 63
-					
-);
+const cardArray6 = [
+	
+	32, 33, 34, 35, 
+	36, 37, 38, 39, 
+	40, 41, 42, 43, 
+	44, 45, 46, 47, 
+	48, 49, 50, 51, 
+	52, 53, 54, 55, 
+	56, 57, 58, 59, 
+	60, 61, 62, 63
+	
+	];
 
 window.onload = function loadGame() {
 	
 	initialize();
 	
-	console.log(`@loadGame(): cardArray1: ${cardArray1}`);
-	console.log(`@loadGame(): cardArray2: ${cardArray2}`);
-	console.log(`@loadGame(): cardArray3: ${cardArray3}`);
-	console.log(`@loadGame(): cardArray4: ${cardArray4}`);
-	console.log(`@loadGame(): cardArray5: ${cardArray5}`);
-	console.log(`@loadGame(): cardArray6: ${cardArray6}`);
+	//console.log(`@loadGame(): cardArray1: ${cardArray1}`);
+	//console.log(`@loadGame(): cardArray2: ${cardArray2}`);
+	//console.log(`@loadGame(): cardArray3: ${cardArray3}`);
+	//console.log(`@loadGame(): cardArray4: ${cardArray4}`);
+	//console.log(`@loadGame(): cardArray5: ${cardArray5}`);
+	//console.log(`@loadGame(): cardArray6: ${cardArray6}`);
 	
 }
 
 function initialize() {
 	
-	const version = 20;
+	const version = 105;
 	console.log(`@initialize(): version: ${version}`);
 	
 	var level = 1;
-	console.log(`@getCardArrayValues(): level: ${level}`);
+	//console.log(`@getCardArrayValues(): level: ${level}`);
 	
 	var sumFirstNumbers = 0;
 	var secretNumber = 0;
@@ -126,7 +132,7 @@ function initialize() {
 	
 	function getCardArrayValues(numberExists) {
 		
-		console.log(`@getCardArrayValues(): level: ${level}`);
+		//console.log(`@getCardArrayValues(): level: ${level}`);
 		
 		switch (level) {
 			
@@ -136,46 +142,41 @@ function initialize() {
 				break;
 			case 2:
 				sumFirstNumbers = numberExists ? sumFirstNumbers + cardArray1[0] : sumFirstNumbers;
-				console.log(`@getCardArrayValues(): sumFirstNumbers: ${sumFirstNumbers}`);
+				//console.log(`@getCardArrayValues(): sumFirstNumbers: ${sumFirstNumbers}`);
 				htmlOutput(cardArray2);
 				level++;
 				break;
 			case 3:
 				sumFirstNumbers = numberExists ? sumFirstNumbers + cardArray2[0] : sumFirstNumbers;
-				console.log(`@getCardArrayValues(): sumFirstNumbers: ${sumFirstNumbers}`);
+				//console.log(`@getCardArrayValues(): sumFirstNumbers: ${sumFirstNumbers}`);
 				htmlOutput(cardArray3);
 				level++;
 				break;
 			case 4:
 				sumFirstNumbers = numberExists ? sumFirstNumbers + cardArray3[0] : sumFirstNumbers;
-				console.log(`@getCardArrayValues(): sumFirstNumbers: ${sumFirstNumbers}`);
+				//console.log(`@getCardArrayValues(): sumFirstNumbers: ${sumFirstNumbers}`);
 				htmlOutput(cardArray4);
 				level++;
 				break;
 			case 5:
 				sumFirstNumbers = numberExists ? sumFirstNumbers + cardArray4[0] : sumFirstNumbers;
-				console.log(`@getCardArrayValues(): sumFirstNumbers: ${sumFirstNumbers}`);
+				//console.log(`@getCardArrayValues(): sumFirstNumbers: ${sumFirstNumbers}`);
 				htmlOutput(cardArray5);
 				level++;
 				break;
 			case 6:
 				sumFirstNumbers = numberExists ? sumFirstNumbers + cardArray5[0] : sumFirstNumbers;
-				console.log(`@getCardArrayValues(): sumFirstNumbers: ${sumFirstNumbers}`);
+				//console.log(`@getCardArrayValues(): sumFirstNumbers: ${sumFirstNumbers}`);
 				htmlOutput(cardArray6);
 				level++;
 				break;
 			case 7:
 				sumFirstNumbers = numberExists ? sumFirstNumbers + cardArray6[0] : sumFirstNumbers;
-				console.log(`@getCardArrayValues(): sumFirstNumbers: ${sumFirstNumbers}`);
+				//console.log(`@getCardArrayValues(): sumFirstNumbers: ${sumFirstNumbers}`);
 				secretNumber = sumFirstNumbers;
-				console.log(`@getCardArrayValues(): secretNumber: ${secretNumber}`);
+				//console.log(`@getCardArrayValues(): secretNumber: ${secretNumber}`);
 				document.getElementById('showCards').innerHTML =
-					'<tr><td>'
-					+ JSON.stringify(
-									'Die Zahl lautet: '
-									+ secretNumber, null, 4
-									)
-									+ '</td></tr>';
+					`Du hast Dir die Zahl ${secretNumber} ausgedacht!`;
 				level = 1;
 				sumFirstNumbers = 0;
 				secretNumber = 0;
@@ -186,19 +187,22 @@ function initialize() {
 	
 }
 
-function magicGuessing() {
+function sortedArrayString(array) {
 	
+	array.sort(sortNumber);
 	
+	function sortNumber(a, b) {
+		
+	    return a - b;
+	    
+	}
+	
+	return outputString = array.join(", ");
 	
 }
 
-function htmlOutput(cardArrayValues) {
+function htmlOutput(input) {
 	
-	return document.getElementById('showCards').innerHTML =
-		'<tr><td>'
-		+ JSON.stringify(
-		                 cardArrayValues, null, 4
-		                 )
-		                 + '</td></tr>';
+	return document.getElementById('showCards').innerHTML = input.join(", ");
 	
 }
