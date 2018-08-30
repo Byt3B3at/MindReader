45435bf4e8f32e44b9ed7573e906691a4c1fb233
Marcus <marcus.freund@web.de>
2018-08-30 22:45:13 +0200

    javascript rework
    - MagicCard class added
    - cleanup

--- MagischeKarten/script.js
+++ MagischeKarten/script.js
@@ -3,7 +3,7 @@
  * 
  * @author Marcus Freund
  * @date 2018-08-06
- * @version 1.5
+ * @version 1.55
  * @description This library offers functions run a magic card game. The game
  *              works as follows: The player is told to think of a number
  *              between 1 and 63. The game starts with a card which displays 32
@@ -16,19 +16,205 @@
 
 const LOGGING_ENABLED = false;
 
-let array1, array2, array3, array4, array5, array6;
-let array1Table, array2Table, array3Table, array4Table, array5Table, array6Table;
+class MagicCard {
+    
+    constructor(cardNumbers, htmlElementId) {
+        
+        this.cardNumbers = cardNumbers;
+        this.htmlElementId = htmlElementId;
+        
+    }
+
+    get tableArray() {
+        
+        return this.createTableArray();
+        
+    }
+
+    createTableArray() {
+
+        let table = document.createElement('table');
+        table.setAttribute('class', 'table');
+        table.setAttribute('id', this.htmlElementId);
+        
+        let tableBody = document.createElement('tbody');
+        
+        this.cardNumbers.forEach(function(rowData) {
+
+            let row = document.createElement('tr');
+            
+            rowData.forEach(function(cellData) {
+
+                let cell = document.createElement('td');
+                cell.appendChild(document.createTextNode(cellData));
+                row.appendChild(cell);
+            });
+            tableBody.appendChild(row);
+        });
+        table.appendChild(tableBody);
+        
+        return table;
+    }
+    
+ }
+
 let buttonYes, buttonNo, buttonAgain;
+
+const card1 = getCard1(getNumbersCard1());
+const card2 = getCard2(getNumbersCard2());
+const card3 = getCard3(getNumbersCard3());
+const card4 = getCard4(getNumbersCard4());
+const card5 = getCard5(getNumbersCard5());
+const card6 = getCard6(getNumbersCard6());
+
 let level;
+
 let paragraph1, paragraph2;
+
 let secretNumber;
 
 function initialize() {
 
-    setArrays();
-    setButtons();
     setParagraphs();
-    setTables();
+    setButtons();
+    
+}
+
+function sliceArray(array){
+    
+    return [
+        [
+            array.slice(0, 4).join(', ')
+        ], [
+            array.slice(4, 8).join(', ')
+        ], [
+            array.slice(8, 12).join(', ')
+        ], [
+            array.slice(12, 16).join(', ')
+        ], [
+            array.slice(16, 20).join(', ')
+        ], [
+            array.slice(20, 24).join(', ')
+        ], [
+            array.slice(24, 28).join(', ')
+        ], [
+            array.slice(28).join(', ')
+        ]
+    ];
+    
+}
+
+function getNumbersCard1() {
+    
+    const array =
+        [
+            
+            '01', '03', '05', '07', '09', '11', '13', '15', '17', '19', '21', '23', '25', '27',
+            '29', '31', '33', '35', '37', '39', '41', '43', '45', '47', '49', '51', '53', '55',
+            '57', '59', '61', '63'
+        
+        ];
+    return array;
+}
+
+function getCard1(numbers) {
+    
+    return new MagicCard(sliceArray(numbers), 'arrayTable1').tableArray;
+    
+}
+
+function getNumbersCard2() {
+    
+    const array =
+        [
+            
+            '02', '03', '06', '07', '10', '11', '14', '15', '18', '19', '22', '23', '26', '27',
+            '30', '31', '34', '35', '38', '39', '42', '43', '46', '47', '50', '51', '54', '55',
+            '58', '59', '62', '63'
+        
+        ];
+    return array;
+}
+
+function getCard2(numbers) {
+    
+    return new MagicCard(sliceArray(numbers), 'arrayTable2').tableArray;
+    
+}
+
+function getNumbersCard3() {
+    
+    const array =
+        [
+            
+            '04', '05', '06', '07', '12', '13', '14', '15', '20', '21', '22', '23', '28', '29',
+            '30', '31', '36', '37', '38', '39', '44', '45', '46', '47', '52', '53', '54', '55',
+            '60', '61', '62', '63'
+        
+        ];
+    return array;
+}
+
+function getCard3(numbers) {
+    
+    return new MagicCard(sliceArray(numbers), 'arrayTable3').tableArray;
+    
+}
+
+function getNumbersCard4() {
+    
+    const array =
+        [
+            
+            '08', '09', '10', '11', '12', '13', '14', '15', '24', '25', '26', '27', '28', '29',
+            '30', '31', '40', '41', '42', '43', '44', '45', '46', '47', '56', '57', '58', '59',
+            '60', '61', '62', '63'
+        
+        ];
+    return array;
+}
+
+function getCard4(numbers) {
+    
+    return new MagicCard(sliceArray(numbers), 'arrayTable4').tableArray;
+    
+}
+    
+function getNumbersCard5() {
+    
+    const array =
+        [
+            
+            '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
+            '30', '31', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59',
+            '60', '61', '62', '63'
+        
+        ];
+    return array;
+}
+
+function getCard5(numbers) {
+    
+    return new MagicCard(sliceArray(numbers), 'arrayTable5').tableArray;
+    
+}
+    
+function getNumbersCard6() {
+    
+    const array =
+        [
+            
+            '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45',
+            '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59',
+            '60', '61', '62', '63'
+        
+        ];
+    return array;
+}
+
+function getCard6(numbers) {
+    
+    return new MagicCard(sliceArray(numbers), 'arrayTable6').tableArray;
     
 }
 
@@ -49,9 +235,9 @@ function setParagraphs() {
         `Denke Dir nun bitte eine Zahl zwischen 1 und 63
         aus und beantworte mir, ob sie sich auf der unten
         dargestellten sowie den folgenden Karten (insgesamt 6)
-        auftaucht:`)
+        auftaucht:`);
     paragraph2 = createParagraph('exerciseQuestion', 
-        `Ist Deine ausgedachte Zahl auf der obigen Karte dabei?`)
+        `Ist Deine ausgedachte Zahl auf der obigen Karte dabei?`);
     
 }
 
@@ -75,227 +261,20 @@ function setButtons() {
     
 }
 
-function setArrays() {
-
-    array1 =
-        [
-            
-            '01', '03', '05', '07', '09', '11', '13', '15', '17', '19', '21', '23', '25', '27',
-            '29', '31', '33', '35', '37', '39', '41', '43', '45', '47', '49', '51', '53', '55',
-            '57', '59', '61', '63'
-        
-        ];
-    
-    array2 =
-        [
-            
-            '02', '03', '06', '07', '10', '11', '14', '15', '18', '19', '22', '23', '26', '27',
-            '30', '31', '34', '35', '38', '39', '42', '43', '46', '47', '50', '51', '54', '55',
-            '58', '59', '62', '63'
-        
-        ];
-    
-    array3 =
-        [
-            
-            '04', '05', '06', '07', '12', '13', '14', '15', '20', '21', '22', '23', '28', '29',
-            '30', '31', '36', '37', '38', '39', '44', '45', '46', '47', '52', '53', '54', '55',
-            '60', '61', '62', '63'
-        
-        ];
-    
-    array4 =
-        [
-            
-            '08', '09', '10', '11', '12', '13', '14', '15', '24', '25', '26', '27', '28', '29',
-            '30', '31', '40', '41', '42', '43', '44', '45', '46', '47', '56', '57', '58', '59',
-            '60', '61', '62', '63'
-        
-        ];
-    
-    array5 =
-        [
-            
-            '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
-            '30', '31', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59',
-            '60', '61', '62', '63'
-        
-        ];
-    
-    array6 =
-        [
-            
-            '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45',
-            '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59',
-            '60', '61', '62', '63'
-        
-        ];
-    
-}
-
-function createTable(tableData, id) {
-
-    let table = document.createElement('table');
-    table.setAttribute('class', 'table');
-    table.setAttribute('id', id);
-    
-    let tableBody = document.createElement('tbody');
-    
-    tableData.forEach(function(rowData) {
-
-        var row = document.createElement('tr');
-        
-        rowData.forEach(function(cellData) {
-
-            var cell = document.createElement('td');
-            cell.appendChild(document.createTextNode(cellData));
-            row.appendChild(cell);
-        });
-        tableBody.appendChild(row);
-    });
-    table.appendChild(tableBody);
-    
-    return table;
-}
-
-function setTables() {
-
-    array1Table = createTable([
-        [
-            array1.slice(0, 4).join(', ')
-        ], [
-            array1.slice(4, 8).join(', ')
-        ], [
-            array1.slice(8, 12).join(', ')
-        ], [
-            array1.slice(12, 16).join(', ')
-        ], [
-            array1.slice(16, 20).join(', ')
-        ], [
-            array1.slice(20, 24).join(', ')
-        ], [
-            array1.slice(24, 28).join(', ')
-        ], [
-            array1.slice(28).join(', ')
-        ]
-    ], 'array1Table');
-    
-    array2Table = createTable([
-        [
-            array2.slice(0, 4).join(', ')
-        ], [
-            array2.slice(4, 8).join(', ')
-        ], [
-            array2.slice(8, 12).join(', ')
-        ], [
-            array2.slice(12, 16).join(', ')
-        ], [
-            array2.slice(16, 20).join(', ')
-        ], [
-            array2.slice(20, 24).join(', ')
-        ], [
-            array2.slice(24, 28).join(', ')
-        ], [
-            array2.slice(28).join(', ')
-        ]
-    ], 'array2Table');
-    
-    array3Table = createTable([
-        [
-            array3.slice(0, 4).join(', ')
-        ], [
-            array3.slice(4, 8).join(', ')
-        ], [
-            array3.slice(8, 12).join(', ')
-        ], [
-            array3.slice(12, 16).join(', ')
-        ], [
-            array3.slice(16, 20).join(', ')
-        ], [
-            array3.slice(20, 24).join(', ')
-        ], [
-            array3.slice(24, 28).join(', ')
-        ], [
-            array3.slice(28).join(', ')
-        ]
-    ], 'array3Table');
-    
-    array4Table = createTable([
-        [
-            array4.slice(0, 4).join(', ')
-        ], [
-            array4.slice(4, 8).join(', ')
-        ], [
-            array4.slice(8, 12).join(', ')
-        ], [
-            array4.slice(12, 16).join(', ')
-        ], [
-            array4.slice(16, 20).join(', ')
-        ], [
-            array4.slice(20, 24).join(', ')
-        ], [
-            array4.slice(24, 28).join(', ')
-        ], [
-            array4.slice(28).join(', ')
-        ]
-    ], 'array4Table');
-    
-    array5Table = createTable([
-        [
-            array5.slice(0, 4).join(', ')
-        ], [
-            array5.slice(4, 8).join(', ')
-        ], [
-            array5.slice(8, 12).join(', ')
-        ], [
-            array5.slice(12, 16).join(', ')
-        ], [
-            array5.slice(16, 20).join(', ')
-        ], [
-            array5.slice(20, 24).join(', ')
-        ], [
-            array5.slice(24, 28).join(', ')
-        ], [
-            array5.slice(28).join(', ')
-        ]
-    ], 'array5Table');
-    
-    array6Table = createTable([
-        [
-            array6.slice(0, 4).join(', ')
-        ], [
-            array6.slice(4, 8).join(', ')
-        ], [
-            array6.slice(8, 12).join(', ')
-        ], [
-            array6.slice(12, 16).join(', ')
-        ], [
-            array6.slice(16, 20).join(', ')
-        ], [
-            array6.slice(20, 24).join(', ')
-        ], [
-            array6.slice(24, 28).join(', ')
-        ], [
-            array6.slice(28).join(', ')
-        ]
-    ], 'array6Table');
-    
-}
-
-function getArrayValues(htmlElementId, userInput) {
+function runGame(htmlElementId, userInput) {
 
     switch (getLevel()) {
         
         case 1:
-            setSecretNumber(null, false);
+            setSecretNumber(htmlElementId, 0);
             showHtmlElement(paragraph1);
             if (!getHtmlElementById(htmlElementId).childNodes[0]) {
                 
-                appendChildElement(htmlElementId, array1Table);
+                appendChildElement(htmlElementId, card1);
                 
             } else {
                 
-                replaceChildElement(htmlElementId, array1Table);
+                replaceChildElement(htmlElementId, card1);
                 
             }
             showHtmlElement(paragraph2);
@@ -306,37 +285,37 @@ function getArrayValues(htmlElementId, userInput) {
             break;
         
         case 2:
-            setSecretNumber(userInput, array1);
-            replaceChildElement(htmlElementId, array2Table);
+            setSecretNumber(userInput, getNumbersCard1()[0]);
+            replaceChildElement(htmlElementId, card2);
             setLevel(); // 3
             break;
         
         case 3:
-            setSecretNumber(userInput, array2);
-            replaceChildElement(htmlElementId, array3Table);
+            setSecretNumber(userInput, getNumbersCard2()[0]);
+            replaceChildElement(htmlElementId, card3);
             setLevel(); // 4
             break;
         
         case 4:
-            setSecretNumber(userInput, array3);
-            replaceChildElement(htmlElementId, array4Table);
+            setSecretNumber(userInput, getNumbersCard3()[0]);
+            replaceChildElement(htmlElementId, card4);
             setLevel(); // 5
             break;
         
         case 5:
-            setSecretNumber(userInput, array4);
-            replaceChildElement(htmlElementId, array5Table);
+            setSecretNumber(userInput, getNumbersCard4()[0]);
+            replaceChildElement(htmlElementId, card5);
             setLevel(); // 6
             break;
         
         case 6:
-            setSecretNumber(userInput, array5);
-            replaceChildElement(htmlElementId, array6Table);
+            setSecretNumber(userInput, getNumbersCard5()[0]);
+            replaceChildElement(htmlElementId, card6);
             setLevel(); // 7
             break;
         
         case 7:
-            setSecretNumber(userInput, array6);
+            setSecretNumber(userInput, getNumbersCard6()[0]);
             hideHtmlElement(paragraph1);
             hideHtmlElement(paragraph2);
             hideHtmlElement(buttonYes);
@@ -344,18 +323,19 @@ function getArrayValues(htmlElementId, userInput) {
             showHtmlElement(buttonAgain);
             replaceChildElement(htmlElementId, getResult());
             setLevel(); // 1
+            break;
             
     }
     
     if (LOGGING_ENABLED) {
         
-        console.log(`@getArrayValues(): level: ${getLevel()}`);
+        console.log(`@runGame(): level: ${getLevel()}`);
         
     }
     
     if (LOGGING_ENABLED) {
         
-        console.log(`@getArrayValues(): secretNumber = ${getSecretNumber()}`);
+        console.log(`@runGame(): secretNumber = ${getSecretNumber()}`);
         
     }
     
@@ -384,13 +364,19 @@ function setLevel() {
 
 function getLevel() {
 
-    return !level ? level = 1 : level;
+    return !level ? (level = 1) : level;
     
 }
 
-function setSecretNumber(userInputYes, array) {
+function setSecretNumber(userInputYes, number) {
 
-    return (userInputYes && array) ? secretNumber += (Number(array[0])) : getSecretNumber();
+    if (LOGGING_ENABLED) {
+        
+        console.log(`@setSecretNumber(): number: ${Number(number)}`);
+        
+    }
+    
+    return userInputYes && Number(number) ? secretNumber += Number(number) : getSecretNumber();
     
 }
 
@@ -419,15 +405,6 @@ function appendChildElement(htmlElementId, childElement) {
     
 }
 
-function replaceFirstChildNode(htmlElementId, newChildNode) {
-
-    const htmlElement = getHtmlElementById(htmlElementId);
-    const firstChildNode = htmlElement.childNodes[0];
-    
-    return firstChildNode.replaceChild(newChildNode, firstChildNode.childNodes[0]);
-    
-}
-
 function replaceChildElement(htmlElementId, newChildElement) {
 
     const htmlElement = getHtmlElementById(htmlElementId);
@@ -443,7 +420,7 @@ function showHtmlElement(htmlElementId) {
     
     if (LOGGING_ENABLED) {
         
-        console.log(htmlElement);
+        console.log(`@showHtmlElement(): htmlElement: ${htmlElement}`);
     
     }
     
