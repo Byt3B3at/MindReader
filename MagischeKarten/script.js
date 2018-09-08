ca9ce366948b37ae0a42d98ec125a6fa169d3d5c
Marcus Eff <mfausb@gmail.com>
2018-09-08 13:33:15 +0200

    General, HTML, JavaScript and CSS rework (using Visual Studio Code as IDE now)
    General:
    - created folder for images (img)
    - created folder for libraries (libs)
    
    HTML:
    - auto-formatted code using Visual Studio Code built-in code formatter
    - redefined meta keywords
    - added jQuery library (cdn=google) for later usage
    - added fallback for jQuery (local copy)
    - added resource integrity checks for the jQuery  library, main script library and css stylesheet
    - added instruction how to enable JavaScript if it is disabled or not working
    
    JavaScript:
    - auto-formatted code using Visual Studio Code built-in code formatter
    - removed additional functions to append and replace html child elements
    - added getter and setter for class "MagicCard" to access class variables
    - slightly reorganized code structure
    
    CSS:
    - auto-formatted code using Visual Studio Code extension "CSS Formatter" by Martin Aeschlimann

--- MagischeKarten/script.js
+++ MagischeKarten/script.js
@@ -1,9 +1,9 @@
 /**
- * Magische Karten
+ * MagicCardsGame library
  * 
  * @author Marcus Freund
  * @date 2018-08-06
- * @version 1.59
+ * @version 1.65
  * @description This library offers functions run a magic card game. The game
  *              works as follows: The player is told to think of a number
  *              between 1 and 63. The game starts with a card which displays 32
@@ -19,57 +19,69 @@ const LOGGING_ENABLED = false;
 const DOCUMENT = document;
 
 class MagicCard {
-    
+
     constructor(htmlElementId, cardNumbers) {
-        
-        this.htmlElementId = htmlElementId;
-        this.cardNumbers = cardNumbers;
-        
+
+        this._htmlElementId = htmlElementId;
+        this._cardNumbers = cardNumbers;
+
+    }
+
+    get cardNumbers() {
+
+        return this._cardNumbers;
+
+    }
+
+    set cardNumbers(numbers) {
+
+        this._cardNumbers = numbers;
+
     }
 
     get cardNumberTable() {
-        
+
         return this.createCardNumberTable();
-        
+
     }
 
     createCardNumberTable() {
 
-        const HTML_ELEMENT_ID = this.htmlElementId;
-        const CARD_NUMBERS = this.cardNumbers;
-        
+        const HTML_ELEMENT_ID = this._htmlElementId;
+        const CARD_NUMBERS = sliceNumbers(this._cardNumbers);
+
         const TABLE = DOCUMENT.createElement('table');
         TABLE.setAttribute('class', 'table');
         TABLE.setAttribute('id', HTML_ELEMENT_ID);
         const TABLE_BODY = DOCUMENT.createElement('tbody');
-        CARD_NUMBERS.forEach(function(rowData) {
+        CARD_NUMBERS.forEach(function (rowData) {
 
             const ROW = DOCUMENT.createElement('tr');
             const ROW_DATA = rowData;
-            ROW_DATA.forEach(function(cellData) {
+            ROW_DATA.forEach(function (cellData) {
 
                 const CELL = DOCUMENT.createElement('td');
                 const CELL_DATA = cellData;
                 CELL.appendChild(DOCUMENT.createTextNode(CELL_DATA));
                 ROW.appendChild(CELL);
-                
+
             });
-            
+
             TABLE_BODY.appendChild(ROW);
-            
+
         });
-        
+
         TABLE.appendChild(TABLE_BODY);
         return TABLE;
-        
-    }
-    
- }
 
-function sliceNumbers(numbers){
-    
+    }
+
+}
+
+function sliceNumbers(numbers) {
+
     const cachedNumbers = numbers;
-    
+
     return [
         [
             cachedNumbers.slice(0, 4).join(', ')
@@ -89,19 +101,11 @@ function sliceNumbers(numbers){
             cachedNumbers.slice(28).join(', ')
         ]
     ];
-    
+
 }
 
-function getCard(id, numbers) {
-    
-    const MAGIC_CARD = new MagicCard(id, sliceNumbers(numbers)).cardNumberTable;
-    
-    return MAGIC_CARD;
-    
-}
+const NUMBERS_CARD_1 =
 
-const NUMBERS_CARD_1 = 
-    
     [
         '01', '03', '05', '07', '09', '11', '13', '15', '17', '19', '21', '23', '25', '27',
         '29', '31', '33', '35', '37', '39', '41', '43', '45', '47', '49', '51', '53', '55',
@@ -109,13 +113,29 @@ const NUMBERS_CARD_1 =
     ];
 
 function getNumbersCard1() {
-    
+
     return NUMBERS_CARD_1;
-    
+
 }
 
-const NUMBERS_CARD_2 = 
-    
+const CARD_1 = new MagicCard('card1', getNumbersCard1());
+
+function getCard1() {
+
+    return CARD_1;
+
+}
+
+const CARD_1_TABLE = getCard1().cardNumberTable;
+
+function getTableCard1() {
+
+    return CARD_1_TABLE;
+
+}
+
+const NUMBERS_CARD_2 =
+
     [
         '02', '03', '06', '07', '10', '11', '14', '15', '18', '19', '22', '23', '26', '27',
         '30', '31', '34', '35', '38', '39', '42', '43', '46', '47', '50', '51', '54', '55',
@@ -123,13 +143,29 @@ const NUMBERS_CARD_2 =
     ];
 
 function getNumbersCard2() {
-    
+
     return NUMBERS_CARD_2;
-    
+
+}
+
+const CARD_2 = new MagicCard('card2', getNumbersCard2());
+
+function getCard2() {
+
+    return CARD_2;
+
+}
+
+const CARD_2_TABLE = getCard2().cardNumberTable;
+
+function getTableCard2() {
+
+    return CARD_2_TABLE;
+
 }
 
 const NUMBERS_CARD_3 =
-    
+
     [
         '04', '05', '06', '07', '12', '13', '14', '15', '20', '21', '22', '23', '28', '29',
         '30', '31', '36', '37', '38', '39', '44', '45', '46', '47', '52', '53', '54', '55',
@@ -137,13 +173,29 @@ const NUMBERS_CARD_3 =
     ];
 
 function getNumbersCard3() {
-    
+
     return NUMBERS_CARD_3;
-    
+
+}
+
+const CARD_3 = new MagicCard('card3', getNumbersCard3());
+
+function getCard3() {
+
+    return CARD_3;
+
+}
+
+const CARD_3_TABLE = getCard3().cardNumberTable;
+
+function getTableCard3() {
+
+    return CARD_3_TABLE;
+
 }
 
 const NUMBERS_CARD_4 =
-    
+
     [
         '08', '09', '10', '11', '12', '13', '14', '15', '24', '25', '26', '27', '28', '29',
         '30', '31', '40', '41', '42', '43', '44', '45', '46', '47', '56', '57', '58', '59',
@@ -151,9 +203,25 @@ const NUMBERS_CARD_4 =
     ];
 
 function getNumbersCard4() {
-    
+
     return NUMBERS_CARD_4;
-    
+
+}
+
+const CARD_4 = new MagicCard('card4', getNumbersCard4());
+
+function getCard4() {
+
+    return CARD_4;
+
+}
+
+const CARD_4_TABLE = getCard4().cardNumberTable;
+
+function getTableCard4() {
+
+    return CARD_4_TABLE;
+
 }
 
 const NUMBERS_CARD_5 =
@@ -164,11 +232,27 @@ const NUMBERS_CARD_5 =
     ];
 
 function getNumbersCard5() {
-    
+
     return NUMBERS_CARD_5;
-    
+
 }
-    
+
+const CARD_5 = new MagicCard('card5', getNumbersCard5());
+
+function getCard5() {
+
+    return CARD_5;
+
+}
+
+const CARD_5_TABLE = getCard5().cardNumberTable;
+
+function getTableCard5() {
+
+    return CARD_5_TABLE;
+
+}
+
 const NUMBERS_CARD_6 =
     [
         '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45',
@@ -177,68 +261,35 @@ const NUMBERS_CARD_6 =
     ];
 
 function getNumbersCard6() {
-    
+
     return NUMBERS_CARD_6;
-    
+
 }
 
-
-const CARD_1 = getCard('card1', getNumbersCard1());
-
-function getCard1() {
-    
-    return CARD_1;
-    
-}
-
-const CARD_2 = getCard('card2', getNumbersCard2());
-
-function getCard2() {
-    
-    return CARD_2;
-    
-}
-
-const CARD_3 = getCard('card3', getNumbersCard3());
-
-function getCard3() {
-    
-    return CARD_3;
-    
-}
-
-const CARD_4 = getCard('card4', getNumbersCard4());
-
-function getCard4() {
-    
-    return CARD_4;
-    
-}
-
-const CARD_5 = getCard('card5', getNumbersCard5());
-
-function getCard5() {
-    
-    return CARD_5;
-    
-}
-
-const CARD_6 = getCard('card6', getNumbersCard6());
+const CARD_6 = new MagicCard('card6', getNumbersCard6());
 
 function getCard6() {
-    
+
     return CARD_6;
-    
+
+}
+
+const CARD_6_TABLE = getCard6().cardNumberTable;
+
+function getTableCard6() {
+
+    return CARD_6_TABLE;
+
 }
 
 function createParagraphElement(id, string) {
-    
+
     const PARAGRAPH = DOCUMENT.createElement('p');
     PARAGRAPH.setAttribute('class', 'p');
     PARAGRAPH.setAttribute('id', id);
     PARAGRAPH.appendChild(DOCUMENT.createTextNode(string));
     return PARAGRAPH;
-    
+
 }
 
 const INTRO = `Denke Dir nun bitte eine Zahl zwischen 1 und 63
@@ -261,35 +312,35 @@ function createButtonElement(id, string) {
     BUTTON.setAttribute('type', 'button');
     BUTTON.appendChild(DOCUMENT.createTextNode(string));
     return BUTTON;
-    
+
 }
 
 const BUTTON_YES = createButtonElement('buttonYes', 'Ja');
 const BUTTON_NO = createButtonElement('buttonNo', 'Nein');
 const BUTTON_AGAIN = createButtonElement('buttonAgain', 'Nochmal');
 
-const LEVEL_COUNTER = function() {
-    
+const LEVEL_COUNTER = function () {
+
     let count = 1;
 
-    this.getCount = function() {
-        
+    this.getCount = function () {
+
         return count;
-        
+
     };
 
-    this.setCount = function(value) {
-        
+    this.setCount = function (value) {
+
         return count = value;
-        
+
     };
 
-    this.increase = function() {
-        
+    this.increase = function () {
+
         count++;
-        
+
     };
-   
+
 };
 
 const LEVEL = new LEVEL_COUNTER();
@@ -297,43 +348,43 @@ const LEVEL = new LEVEL_COUNTER();
 function getLevel() {
 
     return LEVEL.getCount();
-    
+
 }
 
 function setLevel(value) {
 
     return LEVEL.setCount(value);
-    
+
 }
 
 function increaseLevel() {
 
     return LEVEL.increase();
-    
+
 }
 
-const SECRET_NUMBER_CALCULATOR = function() {
-    
+const SECRET_NUMBER_CALCULATOR = function () {
+
     let count = 0;
 
-    this.getCount = function() {
-        
+    this.getCount = function () {
+
         return count;
-        
+
     };
 
-    this.setCount = function(value) {
-        
+    this.setCount = function (value) {
+
         return count = value;
-        
+
     };
 
-    this.increase = function(number) {
-        
+    this.increase = function (number) {
+
         count += Number(number);
-        
+
     };
-   
+
 };
 
 const SECRET_NUMBER = new SECRET_NUMBER_CALCULATOR();
@@ -341,42 +392,42 @@ const SECRET_NUMBER = new SECRET_NUMBER_CALCULATOR();
 function getSecretNumber() {
 
     return SECRET_NUMBER.getCount();
-    
+
 }
 
 function setSecretNumber(value) {
 
     return SECRET_NUMBER.setCount(value);
-    
+
 }
 
 function increaseSecretNumber(number) {
 
     return SECRET_NUMBER.increase(number);
-    
+
 }
 
 function runGame(htmlElement, userInputYes) {
 
-    const LEVEL = getLevel();    
+    const LEVEL = getLevel();
     switch (LEVEL) {
-        
+
         case 1:
             if (LOGGING_ENABLED) {
-                
+
                 console.clear();
-                
+
             }
             setSecretNumber(0);
             showHtmlElement(PARAGRAPH_INTRO);
-            if (!htmlElement.childNodes[0]) {
-                
-                appendChildElement(htmlElement, getCard1());
-                
+            if (!htmlElement.hasChildNodes()) {
+
+                htmlElement.appendChild(getTableCard1());
+
             } else {
-                
-                replaceChildElement(htmlElement, getCard1());
-                
+
+                htmlElement.replaceChild(getTableCard1(), htmlElement.childNodes[0]);
+
             }
             showHtmlElement(PARAGRAPH_OUTRO);
             showHtmlElement(BUTTON_YES);
@@ -384,151 +435,137 @@ function runGame(htmlElement, userInputYes) {
             hideHtmlElement(BUTTON_AGAIN);
             increaseLevel(); // 2
             break;
-        
+
         case 2:
             if (userInputYes) {
-                
-                increaseSecretNumber(getNumbersCard1()[0]);
-                
+
+                increaseSecretNumber(getCard1().cardNumbers[0]);
+
             }
-            replaceChildElement(htmlElement, getCard2());
+            htmlElement.replaceChild(getTableCard2(), htmlElement.childNodes[0]);
             increaseLevel(); // 3
             break;
-        
+
         case 3:
             if (userInputYes) {
-                
-                increaseSecretNumber(getNumbersCard2()[0]);
-            
+
+                increaseSecretNumber(getCard2().cardNumbers[0]);
+
             }
-            replaceChildElement(htmlElement, getCard3());
+            htmlElement.replaceChild(getTableCard3(), htmlElement.childNodes[0]);
             increaseLevel(); // 4
             break;
-        
+
         case 4:
             if (userInputYes) {
-                
-                increaseSecretNumber(getNumbersCard3()[0]);
-                
+
+                increaseSecretNumber(getCard3().cardNumbers[0]);
+
             }
-            replaceChildElement(htmlElement, getCard4());
+            htmlElement.replaceChild(getTableCard4(), htmlElement.childNodes[0]);
             increaseLevel(); // 5
             break;
-        
+
         case 5:
             if (userInputYes) {
-                
-                increaseSecretNumber(getNumbersCard4()[0]);
-                
+
+                increaseSecretNumber(getCard4().cardNumbers[0]);
+
             }
-            replaceChildElement(htmlElement, getCard5());
+            htmlElement.replaceChild(getTableCard5(), htmlElement.childNodes[0]);
             increaseLevel(); // 6
             break;
-        
+
         case 6:
             if (userInputYes) {
-                
-                increaseSecretNumber(getNumbersCard5()[0]);
-                
+
+                increaseSecretNumber(getCard5().cardNumbers[0]);
+
             }
-            replaceChildElement(htmlElement, getCard6());
+            htmlElement.replaceChild(getTableCard6(), htmlElement.childNodes[0]);
             increaseLevel(); // 7
             break;
-        
+
         case 7:
             if (userInputYes) {
-                
-                increaseSecretNumber(getNumbersCard6()[0]);
-                
+
+                increaseSecretNumber(getCard6().cardNumbers[0]);
+
             }
             hideHtmlElement(PARAGRAPH_INTRO);
             hideHtmlElement(PARAGRAPH_OUTRO);
             hideHtmlElement(BUTTON_YES);
             hideHtmlElement(BUTTON_NO);
             showHtmlElement(BUTTON_AGAIN);
-            replaceChildElement(htmlElement, getResult());
+            htmlElement.replaceChild(getResult(), htmlElement.childNodes[0]);
             setLevel(1);
             break;
-            
+
     }
-    
+
     if (LOGGING_ENABLED) {
-        
+
         console.log(`@runGame(): level: ${LEVEL}`);
-        
+
     }
-    
+
     if (LOGGING_ENABLED) {
-        
+
         console.log(`@runGame(): secretNumber = ${getSecretNumber()}`);
-        
+
     }
-    
+
 }
 
 function getResult() {
-    
+
     const RESULT = `Du hast Dir die Zahl ${getSecretNumber()} ausgedacht!`;
     const PARAGRAPH_RESULT = createParagraphElement('result', RESULT);
 
     if (getSecretNumber() > 0) {
-        
+
         return PARAGRAPH_RESULT;
-    
+
     } else {
-        
+
         return PARAGRAPH_NO_RESULT;
 
     }
-    
-}
 
-function appendChildElement(htmlElement, childElement) {
-
-    return htmlElement.appendChild(childElement);
-    
-}
-
-function replaceChildElement(htmlElement, childElement) {
-
-    const FIRST_CHILD_NODE = htmlElement.childNodes[0];
-    
-    return htmlElement.replaceChild(childElement, FIRST_CHILD_NODE);
-    
 }
 
 function showHtmlElement(htmlElement) {
 
     if (LOGGING_ENABLED) {
-        
+
         console.log(`@showHtmlElement(): htmlElement: ${htmlElement}`);
-    
+
     }
-    
+
     if (htmlElement.getAttribute('type') === 'button') {
-        
+
         return htmlElement.style.display = 'inline-block';
-        
+
     }
-    
+
     return htmlElement.style.visibility = 'visible';
-    
+
 }
 
 function hideHtmlElement(htmlElement) {
 
     if (LOGGING_ENABLED) {
-        
+
         console.log(`@hideHtmlElement(): htmlElement: ${htmlElement}`);
-        
+
     }
-    
+
     if (htmlElement.getAttribute('type') === 'button') {
-        
+
         return htmlElement.style.display = 'none';
-        
+
     }
-    
+
     return htmlElement.style.visibility = 'hidden';
-    
+
 }
